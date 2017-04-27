5d5d4c905df4b5a89ccb75be0ce6b41b24da471e
Mike Pohatu <mikepohatu@gmail.com>
2017-04-28 11:29:48 +1200

    fixed import-module line

--- LogOffInactiveUsers.ps1
+++ LogOffInactiveUsers.ps1
@@ -1,3 +1,3 @@
-﻿Import-Module PcSessionManagement
+﻿Import-Module .\PcSessionManagement.psm1
 
 New-TimeSpan -minutes 15 | Get-IdleUsers  | ForEach-Object { LogoffComputerSessionId -Id $_.Id -ComputerName $_.ComputerName }
